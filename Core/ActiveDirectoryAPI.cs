b41ca589785e06a781c173d5c06640c3e8698eb0
natha <natha@WIN11-M-001>
2026-01-31 14:54:34 -0500

    Add project files.

---
+++ Core/ActiveDirectoryAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ActiveDirectoryManager.SystemTools;
+
+namespace ActiveDirectoryManager.Core
+{
+    class ActiveDirectoryAPI
+    {
+        NetManager netManager;
+
+        // Constructor
+        public ActiveDirectoryAPI() 
+        {
+            netManager = new();
+        }
+        
+         
+    }
+}
---
+++ SystemTools/NetManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryManager.SystemTools
+{
+    class NetManager // Performs simple system-level networking functions
+    {
+        public bool useDNS;
+        NetManager() 
+        {
+            useDNS = true;
+        }
+        
+        // ASYNC ping task with by hostname 
+        public static async Task<bool> ValidateDomainAvailability(string domainName)
+        {
+            using Ping ping = new();
+            try
+            {
+                PingReply re = await ping.SendPingAsync(domainName, 5000);
+                if ((re != null) || re.Status == IPStatus.Success)
+                {
+                    Console.WriteLine($"Successful reply from {domainName}");
+                    Console.WriteLine($"{domainName} ({re.Address})");
+                    Console.WriteLine($"Reply time ({re.RoundtripTime}) ms");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"Ping error: {re.Status}");
+                    return false;
+                }
+            }
+            catch (PingException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
